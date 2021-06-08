6baa64490f9fe2c7d2a192a0855b2976798da52a
Jani Willman <jani@willman.dev>
2021-06-08 19:16:03 +0300

    Added apollo and basic functionality

--- src/App.js
+++ src/App.js
@@ -1,25 +1,59 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import {
+    ApolloClient,
+    InMemoryCache,
+    ApolloProvider,
+    useQuery,
+    gql,
+} from "@apollo/client";
+
+const client = new ApolloClient({
+    uri: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
+    cache: new InMemoryCache(),
+});
+
+const stationId = 133;
+
+const STATION_INFO = gql`
+    query StationInfo {
+        bikeRentalStation(id: "133") {
+            stationId
+            name
+            bikesAvailable
+            spacesAvailable
+            lat
+            lon
+            allowDropoff
+        }
+    }
+`;
+
+function StationInfo() {
+    const { loading, error, data } = useQuery(STATION_INFO);
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    console.log(data);
+
+    return (
+        <>
+            <h1>{data.bikeRentalStation.name}</h1>
+            <p>Pyöriä vapaana: {data.bikeRentalStation.bikesAvailable} </p>
+            <p>Paikkoja vapaana: {data.bikeRentalStation.spacesAvailable}</p>
+        </>
+    );
+}
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <ApolloProvider client={client}>
+                <div className="container">
+                    <StationInfo></StationInfo>
+                </div>
+            </ApolloProvider>
+        </>
+    );
 }
 
 export default App;
