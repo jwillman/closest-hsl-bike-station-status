28c83f9530ab7e2e0478760dc669bbe5203e82a0
Jani Willman <jani@willman.dev>
2021-06-08 19:38:36 +0300

    Own component for StationIndo and parametrized the query

--- src/App.js
+++ src/App.js
@@ -1,55 +1,18 @@
 import "./App.css";
-import {
-    ApolloClient,
-    InMemoryCache,
-    ApolloProvider,
-    useQuery,
-    gql,
-} from "@apollo/client";
-
-const client = new ApolloClient({
-    uri: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
-    cache: new InMemoryCache(),
-});
-
-const stationId = 133;
-
-const STATION_INFO = gql`
-    query StationInfo {
-        bikeRentalStation(id: "133") {
-            stationId
-            name
-            bikesAvailable
-            spacesAvailable
-            lat
-            lon
-            allowDropoff
-        }
-    }
-`;
-
-function StationInfo() {
-    const { loading, error, data } = useQuery(STATION_INFO);
-    if (loading) return <p>Loading...</p>;
-    if (error) return <p>Error :(</p>;
-
-    console.log(data);
-
-    return (
-        <>
-            <h1>{data.bikeRentalStation.name}</h1>
-            <p>Pyöriä vapaana: {data.bikeRentalStation.bikesAvailable} </p>
-            <p>Paikkoja vapaana: {data.bikeRentalStation.spacesAvailable}</p>
-        </>
-    );
-}
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+import StationInfo from "./components/StationInfo.js";
 
 function App() {
+    const client = new ApolloClient({
+        uri: "https://api.digitransit.fi/routing/v1/routers/hsl/index/graphql",
+        cache: new InMemoryCache(),
+    });
+
     return (
         <>
             <ApolloProvider client={client}>
                 <div className="container">
-                    <StationInfo></StationInfo>
+                    <StationInfo stationId="134"></StationInfo>
                 </div>
             </ApolloProvider>
         </>
---
+++ src/components/StationInfo.js
@@ -0,0 +1,33 @@
+import { useQuery, gql } from "@apollo/client";
+
+function StationInfo({ stationId }) {
+    const STATION_INFO = gql`
+        query StationInfo($id: String!) {
+            bikeRentalStation(id: $id) {
+                stationId
+                name
+                bikesAvailable
+                spacesAvailable
+                lat
+                lon
+                allowDropoff
+            }
+        }
+    `;
+
+    const { loading, error, data } = useQuery(STATION_INFO, {
+        variables: { stationId },
+    });
+    if (loading) return <p>Loading...</p>;
+    if (error) return <p>Error :(</p>;
+
+    return (
+        <>
+            <h1>{data.bikeRentalStation.name}</h1>
+            <p>Pyöriä vapaana: {data.bikeRentalStation.bikesAvailable} </p>
+            <p>Paikkoja vapaana: {data.bikeRentalStation.spacesAvailable}</p>
+        </>
+    );
+}
+
+export default StationInfo;
